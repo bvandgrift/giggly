3833780130d668b1533463101abf493a9e1bdeff
Ben Vandgrift <ben@IA-Developer-1.local>
2009-09-14 15:28:12 -0400

    new setup.  whuf.

---
+++ lib/giggly/rest/connection.rb
@@ -0,0 +1,9 @@
+
+module Giggly
+  module Rest
+    class Connection
+      def initialize()
+      end
+    end
+  end
+end
--- tasks/giggly_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :giggly do
-#   # Task goes here
-# end
