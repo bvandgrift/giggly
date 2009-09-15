2a5679c2bbdb6166cfd8462105e9f3f7a0e64780
Adam Hunter <adamhunter@me.com>
2009-09-15 14:48:59 -0400

    added some fun stuff :D

--- lib/giggly/rest/rest.rb
+++ lib/giggly/rest/rest.rb
@@ -3,6 +3,23 @@ reqiure 'digest'
 module Giggly
   module Rest
     
+    class SocializeError < StandardError
+      attr_reader :error_code, :error_description
+
+      def initialize(error_data)
+        @error_code, @error_description = error_data[:error_code], error_data[:description]
+        super
+      end
+    end
+
+    class BadRequest < SocializeError; end
+    class Unauthorized < SocializeError; end
+    class Forbidden < SocializeError; end
+    class NotFound < SocializeError; end
+    class RequestEntityTooLarge < SocializeError; end
+    class InternalServerError < SocializeError; end
+    class NotImplemented < SocializeError; end
+    
     def self.signature(token, value)
       base_string = "#{token}_#{value}"
       binary_key = Base64.decode64(Giggly.config["APIKey"])
--- lib/giggly/rest/socialize.rb
+++ lib/giggly/rest/socialize.rb
@@ -2,8 +2,9 @@ module Giggly
   module Rest
     class Socialize
       
-      def initialize(connection_or_hash)
-        
+      def initialize(connection)
+        @connection = connection and return if connection.kind_of?(Giggly::Rest::Connection)
+        @connection = Giggly::Rest::Connection.new(connection)
       end
       
     end
